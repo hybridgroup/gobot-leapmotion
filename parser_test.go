97233b9e933f13f894d1a2fc8b0030cafa587450
Andrew Stewart <andrew@stwrt.ca>
2013-12-18 13:58:40 -0800

    Modifications so that parser tests pass

--- parser_test.go
+++ parser_test.go
@@ -7,18 +7,21 @@ import (
 )
 
 var _ = Describe("Parser", func() {
+	leapAdaptor := new(LeapAdaptor)
+	leap := NewLeap(leapAdaptor)
+
 	Describe("#ParseLeapFrame", func() {
 		It("Takes an array of bytes and extracts Leap Frames", func() {
 			file, err := ioutil.ReadFile("./test/support/example_frame.json")
 			Expect(err != nil)
-			parsedFrame := ParseLeapFrame(file)
+			parsedFrame := leap.ParseLeapFrame(file)
 			Expect(parsedFrame.Hands != nil)
 			Expect(parsedFrame.Pointables != nil)
 			Expect(parsedFrame.Gestures != nil)
 		})
 
 		It("Returns an empty Leap Frame if passed non-Leap bytes", func() {
-			parsedFrame := ParseLeapFrame([]byte{})
+			parsedFrame := leap.ParseLeapFrame([]byte{})
 			Expect(parsedFrame.Timestamp == 0)
 		})
 	})
